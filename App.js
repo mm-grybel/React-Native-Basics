cb2f6d413a984190df7bb0ff60682460ce98c35b
mgrybel <monika.grybel@hotmail.com>
2021-03-25 00:23:43 +0000

    Update App.js

--- App.js
+++ App.js
@@ -3,21 +3,22 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Hello, World!');
-  return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText('The text has changed!')} />
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [outputText, setOutputText] = useState('Hello, World!');
+    
+    return (
+        <View style={styles.container}>
+            <Text>{outputText}</Text>
+            <Button title="Change Text" onPress={() => setOutputText('The text has changed!')} />
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
